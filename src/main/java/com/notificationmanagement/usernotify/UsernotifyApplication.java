27deb61daeadbe618c654b239bd532de542eb9e9
PRISCILLA ROCKENBACH <priscilla.rockenbach@ericsson.com>
2024-08-21 10:10:03 -0300

    Create Repository

---
+++ src/main/java/com/notificationmanagement/usernotify/UsernotifyApplication.java
@@ -0,0 +1,13 @@
+package com.notificationmanagement.usernotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsernotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsernotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notificationmanagement/usernotify/UsernotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notificationmanagement.usernotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsernotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
